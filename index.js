4511102b22e231fb3f8c5db6a1b89b4598493655
Wator Vapor <watorvapor@gmail.com>
2022-04-29 18:39:46 +0900

    iot port

--- index.js
+++ index.js
@@ -1,169 +1,24 @@
-window.addEventListener('load', (evt) => {
-  tryCreateEd25519Key();
-  onCreateMqttConnection();
-});
-const mqtturl = 'wss://broker.emqx.io:8084/mqtt';
-let gMqttClient = false;
-console.log('::mqtturl:=<', mqtturl, '>');
-const clientID = 'mqttjs_' + Base58.encode(nacl.randomBytes(32))
-console.log('::clientID:=<', clientID, '>');
-const onCreateMqttConnection = () => {
-  const options = {
-    protocolId: 'MQTT',
-    connectTimeout: 30 * 1000,
-    clean: true,
-    reconnectPeriod: 1000,
-    // Authentication
-    clientId: clientID,
-    username: '',
-    password: '',
-    keepalive: 30*1000,
-    clean: true,
-  }
-  gMqttClient = mqtt.connect(mqtturl,options);
-  console.log('onCreateMqttConnection::gMqttClient.connected:=<', gMqttClient.connected, '>');
-  gMqttClient.on('connect', () => {
-    console.log('onCreateMqttConnection::gMqttClient.connected:=<', gMqttClient.connected, '>');
-    const btn = document.getElementById('ui-gofuro-btn');
-    console.log('onCreateMqttConnection::btn:=<', btn, '>');
-    btn.disabled = false;
-    gMqttClient.subscribe(publicKeyB64,{qos:1} );
-  });
-  gMqttClient.on('disconnect', () => {
-    const btn = document.getElementById('ui-gofuro-btn');
-    console.log('onCreateMqttConnection::btn:=<', btn, '>');
-    btn.disabled = true;
-  });
-  gMqttClient.on('error', () => {
-    const btn = document.getElementById('ui-gofuro-btn');
-    console.log('onCreateMqttConnection::btn:=<', btn, '>');
-    btn.disabled = true;
-  });
-  gMqttClient.on('message', (topic, message) => {
-    console.log('onCreateMqttConnection::on message topic:=<', topic, '>');    
-    console.log('onCreateMqttConnection::on message message:=<', message.toString('utf-8'), '>');    
-  });
-}
-
-const secretEdKeyPath = 'gofuro/v2/ed25519/secret';
-const tryCreateEd25519Key = () => {
-  const secretKey = localStorage.getItem(secretEdKeyPath);
-  if(secretKey) {
-    loadEd25519Key(secretKey);
-  } else {
-    const keyPair = nacl.sign.keyPair();
-    console.log('tryCreateEd25519Key::keyPair:=<', keyPair, '>');
-    const secretKey = nacl.util.encodeBase64(keyPair.secretKey);
-    console.log('tryCreateEd25519Key::secretKey:=<', secretKey, '>');
-    localStorage.setItem(secretEdKeyPath,secretKey);
-    loadEd25519Key(secretKey);
-  }
-}
-let secretKeyBin = false;
-let publicKeyB64 = false;
-let publicKeyB58 = false;
-const loadEd25519Key = (secretKey)=> {
-  console.log('loadEd25519Key::secretKey:=<', secretKey, '>');
-  secretKeyBin = nacl.util.decodeBase64(secretKey);
-  console.log('loadEd25519Key::secretKeyBin:=<', secretKeyBin, '>');
-  const keyPair = nacl.sign.keyPair.fromSecretKey(secretKeyBin);
-  console.log('loadEd25519Key::keyPair:=<', keyPair, '>');
-  publicKeyB64 = nacl.util.encodeBase64(keyPair.publicKey);
-  console.log('loadEd25519Key::publicKeyB64:=<', publicKeyB64, '>');
-  publicKeyB58 = Base58.encode(keyPair.publicKey);
-  console.log('loadEd25519Key::publicKeyB58:=<', publicKeyB58, '>');
-
-  const keyElem = document.getElementById('ui-gofuro-public-key');
-  console.log('onCreateMqttConnection::keyElem:=<', keyElem, '>');
-  keyElem.textContent = 'addAuthedkey:<' + publicKeyB64 + '>';
-
-}
 const onUIClickGoFuro = (elem) => {
   console.log('onUIClickGoFuro::elem:=<', elem, '>');
-  const radios = document.getElementsByName('gpio_port');
-  for(const radio of radios) {
-    console.log('onUIClickGoFuro::radio:=<', radio, '>');
-    if (radio.checked) {
-      publishGpio(parseInt(radio.value));
+  const port_radios = document.getElementsByName('gpio_port');
+  let port = false;
+  for(const port_radio of port_radios) {
+    console.log('onUIClickGoFuro::port_radio:=<', port_radio, '>');
+    if (port_radio.checked) {
+      port = parseInt(port_radio.value);
       break;
     }
   }
-}
-
-
-const publishGpio = (gpio) => {
-  console.log('publishGpio::gpio:=<', gpio, '>');
-  const msg = {
-    d_out:{
-      port:gpio,
-      level:true
-    }
-  };
-  const signedMsg = signByEd25519(msg);
-  console.log('publishGpio::signedMsg:=<', signedMsg, '>');
-  console.log('publishGpio::publicKeyB64:=<', publicKeyB64, '>');
-  console.log('publishGpio::gMqttClient.connected:=<', gMqttClient.connected, '>');
-  const allMsg = JSON.stringify(signedMsg);
-  console.log('publishGpio::allMsg:=<', allMsg, '>');
-  console.log('publishGpio::allMsg.length:=<', allMsg.length, '>');
-  publishMqttMsg(allMsg);
-}
-
-const BUFFER_MAX_SIZE = 64;
-const gSendBufferOfMqtt = [];
-const MQTT_SEND_INTERVAL = 1000;
-const publishMqttMsg = (allMsg) => {
-  for(let start = 0;start < allMsg.length;start += BUFFER_MAX_SIZE) {
-    const end = start + BUFFER_MAX_SIZE;
-    if(end > allMsg.length) {
-      const sendBuffer = allMsg.substring(start);
-      const sendMsg = {
-        buff:sendBuffer,
-        finnish:true
-      };
-      const oneBuffer = JSON.stringify(sendMsg);
-      gSendBufferOfMqtt.push(oneBuffer);
-    } else {
-      const sendBuffer = allMsg.substring(start,end);
-      const sendMsg = {
-        buff:sendBuffer,
-        finnish:false
-      };
-      const oneBuffer = JSON.stringify(sendMsg);
-      gSendBufferOfMqtt.push(oneBuffer);
+  const output_radios = document.getElementsByName('gpio_output');
+  let output = false;
+  for(const output_radio of output_radios) {
+    console.log('onUIClickGoFuro::output_radio:=<', output_radio, '>');
+    if (output_radio.checked) {
+      output = parseInt(output_radio.value);
+      break;
     }
   }
-  setTimeout(()=>{
-    publishMsgStepByStep();
-  },MQTT_SEND_INTERVAL);
-}
-const publishMsgStepByStep = ()=> {
-  if(gSendBufferOfMqtt.length > 0) {
-    const oneBuffer = gSendBufferOfMqtt[0];
-    console.log('publishMqttMsg::oneBuffer:=<', oneBuffer, '>');
-    console.log('publishMqttMsg::oneBuffer.length:=<', oneBuffer.length, '>');
-    gMqttClient.publish(publicKeyB64,oneBuffer,{qos:1});
-    gSendBufferOfMqtt.shift();
-    setTimeout(()=>{
-      publishMsgStepByStep();
-    },MQTT_SEND_INTERVAL);    
+  if(port !== false && output !== false) {
+    publishGpio(port,output);
   }
-}
-
-const signByEd25519 = (msg) => {
-  msg.iss = (new Date()).toISOString();
-  const payloadStr = JSON.stringify(msg);
-
-  const encoder = new TextEncoder();
-  const msgHashBin = nacl.hash(encoder.encode(payloadStr));
-  console.log('signByEd25519::msgHashBin:=<', msgHashBin, '>');
-  
-  const signedMsg = {};
-  signedMsg.payload = msg;
-  const signedHash = nacl.sign(msgHashBin, secretKeyBin);
-  signedMsg.auth = {};
-  signedMsg.auth.sha = nacl.util.encodeBase64(msgHashBin);
-  signedMsg.auth.pub = publicKeyB64;
-  signedMsg.auth.sign = nacl.util.encodeBase64(signedHash);
-  return signedMsg;
 }
--- ui.index.js
+++ ui.index.js
@@ -1,9 +1,9 @@
-const KEY_GPIO_SELECTION = 'gofuro2/gpio/selection';
-
 window.addEventListener('load', (evt) => {
-  createApps();
+  createPortApp();
+  createOutputApp();
 });
 
+const KEY_GPIO_PORT_SELECTION = 'gofuro2/gpio/port/selection';
 const gPortApp = {
   gpio:{
     selected:26,
@@ -15,21 +15,21 @@ const loadDefaultOfPortApp = () => {
       gPortApp.gpio.ports.push(i);
   }
 }
-const createApps = () => {
-  const gpioApp = Vue.createApp({
+const createPortApp = () => {
+  const app = Vue.createApp({
     data() {
       try {
-        const lsGpioStr = localStorage.getItem(KEY_GPIO_SELECTION);
-        console.log('createApps::lsGpioStr:=<', lsGpioStr, '>');
+        const lsGpioStr = localStorage.getItem(KEY_GPIO_PORT_SELECTION);
+        console.log('createPortApp::lsGpioStr:=<', lsGpioStr, '>');
         if(lsGpioStr) {
           gPortApp.gpio = JSON.parse(lsGpioStr);
         } else {
           loadDefaultOfPortApp();
-          localStorage.setItem(KEY_GPIO_SELECTION,JSON.stringify(gPortApp.gpio));
+          localStorage.setItem(KEY_GPIO_PORT_SELECTION,JSON.stringify(gPortApp.gpio));
         }
       } catch(err) {
         loadDefaultOfPortApp();
-        localStorage.setItem(KEY_GPIO_SELECTION,JSON.stringify(gPortApp.gpio));
+        localStorage.setItem(KEY_GPIO_PORT_SELECTION,JSON.stringify(gPortApp.gpio));
       }
       return {
         gpio:gPortApp.gpio
@@ -37,11 +37,49 @@ const createApps = () => {
     },
     methods: {
       onChange(evt) {
-        console.log('createApps::evt:=<', evt, '>');
-        localStorage.setItem(KEY_GPIO_SELECTION,JSON.stringify(this.gpio));
+        console.log('createPortApp::evt:=<', evt, '>');
+        localStorage.setItem(KEY_GPIO_PORT_SELECTION,JSON.stringify(this.gpio));
       }
     }
   });
-  gPortApp.vm = gpioApp.mount('#vue-ui-gpio-port');
+  gPortApp.vm = app.mount('#vue-ui-gpio-port');
   console.log('createApps::gPortApp:=<', gPortApp, '>');
 }
+
+const KEY_GPIO_OUTPUT_SELECTION = 'gofuro2/gpio/output/selection';
+const gOutputApp = {
+  gpio:{
+    selected:0,
+    values:[0,1]
+  }
+};
+
+const createOutputApp = () => {
+  const app = Vue.createApp({
+    data() {
+      try {
+        const lsGpioStr = localStorage.getItem(KEY_GPIO_OUTPUT_SELECTION);
+        console.log('createOutputApp::lsGpioStr:=<', lsGpioStr, '>');
+        if(lsGpioStr) {
+          gOutputApp.gpio = JSON.parse(lsGpioStr);
+        } else {
+          localStorage.setItem(KEY_GPIO_OUTPUT_SELECTION,JSON.stringify(gOutputApp.gpio));
+        }
+      } catch(err) {
+        localStorage.setItem(KEY_GPIO_OUTPUT_SELECTION,JSON.stringify(gOutputApp.gpio));
+      }
+      return {
+        gpio:gOutputApp.gpio
+      };
+    },
+    methods: {
+      onChange(evt) {
+        console.log('createOutputApp::evt:=<', evt, '>');
+        localStorage.setItem(KEY_GPIO_OUTPUT_SELECTION,JSON.stringify(this.gpio));
+      }
+    }
+  });
+  gOutputApp.vm = app.mount('#vue-ui-gpio-output');
+  console.log('createOutputApp::gOutputApp:=<', gOutputApp, '>');
+}
+
